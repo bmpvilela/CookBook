7a10bcdb8224ab1d610401ddd16083b366e419ac
brunovilela <bmpvilela@gmail.com>
2025-02-25 12:55:46 +0000

    - Update application.properties - Update models - Update ArtifactID

--- src/main/java/org/example/model/Ingredient.java
+++ src/main/java/org/example/model/Ingredient.java
@@ -1,12 +1,15 @@
 package org.example.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Objects;
 
-//@Entity
+@Entity
 public class Ingredient {
 
-    //@Id
-    //@GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String name;
